9cc30d9365087aa6cb2a8101fb583bec0a9f3e1e
Dmitry Murinov <murinov.dmitry.acc@gmail.com>
2016-05-14 12:03:53 +0300

    Обновил тесты до более правильных по мотивам TestNG

--- JUnitFromTestNG/src/test/java/learn/homeworks/homework_3/CreateFileTest.java
+++ JUnitFromTestNG/src/test/java/learn/homeworks/homework_3/CreateFileTest.java
@@ -8,6 +8,10 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Dima on 03.05.2016.
  */
@@ -19,19 +23,22 @@ public class CreateFileTest extends TestBase {
     public void createEmptyFile() throws IOException {
 
         File file;
+        boolean fileCreated = false;
 
         try {
 
             file = new File(tempDir.getRoot() + "/test1.txt");
             if (!file.exists()) {
-                file.createNewFile();
+                fileCreated = file.createNewFile();
             }
-
-            System.out.println("test 1 done");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        assertThat("File not created", fileCreated, is (true));
+        System.out.println("test 1 done");
+
     }
 
     @Test
@@ -39,7 +46,8 @@ public class CreateFileTest extends TestBase {
     public void createFileWithData() throws IOException {
 
         FileOutputStream fos = null;
-        File file;
+        File file = null;
+        boolean fileCreated = false;
         String content = "This is the text content";
 
         try {
@@ -49,7 +57,7 @@ public class CreateFileTest extends TestBase {
 
             // if file doesnt exists, then create it
             if (!file.exists()) {
-                file.createNewFile();
+                fileCreated = file.createNewFile();
             }
 
             // get the content in bytes
@@ -72,6 +80,11 @@ public class CreateFileTest extends TestBase {
                 e.printStackTrace();
             }
         }
+
+        assertThat("File not created", fileCreated, is (true));
+        long fileSize = 0;
+        if(file != null){fileSize = file.length();}
+        assertTrue(fileSize > 0);
     }
 
     @Test
@@ -79,6 +92,7 @@ public class CreateFileTest extends TestBase {
     public void createFileWrongPath() {
 
         File file;
+        IOException exception = null;
 
         try {
 
@@ -89,7 +103,18 @@ public class CreateFileTest extends TestBase {
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
-            System.out.println("test 3");
+            System.out.println("test 3 copy");
+
+            assertThat("There are no message about wrong path or message in a wrong language",
+                    e.getMessage(), is ("Синтаксическая ошибка в имени файла, имени папки или метке тома"));
+
+            exception = e;
+        } finally {
+            if(!exception.getMessage().equals("Синтаксическая ошибка в имени файла, имени папки или метке тома")){
+                System.out.println("There are no message from system or wrong message");
+                System.out.println("Message from System: " + exception.getMessage());
+                System.exit(1);
+            }
         }
     }
 
--- JUnitFromTestNG/src/test/java/learn/homeworks/homework_3_different_experiments/CreateFileTestTwoProviders.java
+++ JUnitFromTestNG/src/test/java/learn/homeworks/homework_3_different_experiments/CreateFileTestTwoProviders.java
@@ -16,6 +16,7 @@ import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Dima on 03.05.2016.
@@ -53,7 +54,8 @@ public class CreateFileTestTwoProviders extends TestBase {
     public void createFileWithData(FileData filedata) throws IOException {
 
         FileOutputStream fos = null;
-        File file;
+        File file = null;
+        boolean fileCreated = false;
         String content = filedata.getData();
 
         System.out.println(content);
@@ -65,7 +67,7 @@ public class CreateFileTestTwoProviders extends TestBase {
 
             // if file doesnt exists, then create it
             if (!file.exists()) {
-                file.createNewFile();
+                fileCreated = file.createNewFile();
             }
 
             // get the content in bytes
@@ -88,12 +90,18 @@ public class CreateFileTestTwoProviders extends TestBase {
                 e.printStackTrace();
             }
         }
+
+        assertThat("File not created", fileCreated, is (true));
+        long fileSize = 0;
+        if(file != null){fileSize = file.length();}
+        assertTrue(fileSize > 0);
     }
 
     @Test
     public void createFileWrongPath() {
 
         File file;
+        IOException exception = null;
 
         try {
 
@@ -107,7 +115,15 @@ public class CreateFileTestTwoProviders extends TestBase {
             System.out.println("test 3 copy");
 
             assertThat("There are no message about wrong path or message in a wrong language",
-                    e.getMessage(), is("Синтаксическая ошибка в имени файла, имени папки или метке тома"));
+                    e.getMessage(), is ("Синтаксическая ошибка в имени файла, имени папки или метке тома"));
+
+            exception = e;
+        } finally {
+            if(!exception.getMessage().equals("Синтаксическая ошибка в имени файла, имени папки или метке тома")){
+                System.out.println("There are no message from system or wrong message");
+                System.out.println("Message from System: " + exception.getMessage());
+                System.exit(1);
+            }
         }
     }
 
